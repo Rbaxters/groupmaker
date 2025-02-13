ccd843879b60a59498c6eca86572af3e4262b16a
Rbaxters <158490067+Rbaxters@users.noreply.github.com>
2025-02-13 16:06:11 -0500

    Update GroupMaker.py

--- GroupMaker.py
+++ GroupMaker.py
@@ -9,46 +9,48 @@ students = [
 print("Students:")
 print(str(students))
 
-userInput = input("Choose the number of students in each group (2, 3, 4): ")
-
-anyExclusions1 = input("Is there any pairs of students that can't work together? Y/N: ")
-
-anyExclusions1 = anyExclusions1.capitalize()
-
+# Ensure valid group size input (2, 3, or 4)
 while True:
-    if anyExclusions1 == 'Y':
-    
-        while True:
-            excludedStudent1 = input("Please enter the name of the first student: ")
-            excludedStudent1 = excludedStudent1.capitalize()
-            if excludedStudent1 in students:
-                break
-            else:
-                print('Invalid name. Please try again.')
-            
-        while True:
-            excludedStudent2 = input("Please enter the name of the second student: ")
-            excludedStudent2 = excludedStudent2.capitalize()
-            if excludedStudent2 in students:
-                break
-            else:
-                print('Invalid name. Please try again.')
-                
-                
-        anyExclusions2 = input("Is there another pair of Students that cannot work together? Y/N: ")
-
-        anyExclusions2 = anyExclusions2.capitalize()
-            
-    elif anyExclusions1 == 'N':
+    userInput = input("Choose the number of students in each group (2, 3, 4): ")
+    if userInput in ['2', '3', '4']:
         break
-
     else:
-        print("That's an invalid input. Please try again.")
-        
+        print("That is not a valid input. Please try again.")
+
+# Handle exclusion of pairs
+anyExclusions1 = input("Is there any pairs of students that can't work together? Y/N: ").capitalize()
+
+excluded_pairs = []
+
+while anyExclusions1 == 'Y':
+    while True:
+        excludedStudent1 = input("Please enter the name of the first student: ")
+        excludedStudent1 = excludedStudent1.capitalize()
+        if excludedStudent1 in students:
+            break
+        else:
+            print('Invalid name. Please try again.')
+    
+    while True:
+        excludedStudent2 = input("Please enter the name of the second student: ")
+        excludedStudent2 = excludedStudent2.capitalize()
+        if excludedStudent2 in students:
+            break
+        else:
+            print('Invalid name. Please try again.')
+    
+    excluded_pairs.append((excludedStudent1, excludedStudent2))
+
+    anyExclusions2 = input("Is there another pair of Students that cannot work together? Y/N: ").capitalize()
+    
+    if anyExclusions2 == 'N':
+        break
+    elif anyExclusions2 != 'Y':
+        print("Invalid input. Please answer with Y or N.")
+        anyExclusions2 = input("Is there another pair of Students that cannot work together? Y/N: ").capitalize()
 
 
-
-
+# Create groups based on user input
 while True:
     if userInput == '2':
         random.shuffle(students)
@@ -70,7 +72,3 @@ while True:
         for groups in groupsof4:
             print(groups)
         break
-
-    else:
-        print('That is not a valid input. Please try again')
-        userInput = input("Choose the number of students in each group (2, 3, 4): ")
